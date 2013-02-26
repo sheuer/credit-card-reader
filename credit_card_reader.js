fa30464b9e58b867d09f1fb61a5685d9256f9f8e
Stephen Heuer <sheuer@int42.org>
2013-02-25 19:44:05 -0600

    update copyright

--- credit_card_reader.js
+++ credit_card_reader.js
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2012 Stephen Heuer
+Copyright (c) 2013 Stephen Heuer
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
